f6261221ad115040fd3274cc3c94280aea5ae70f
Reversa Nexo <julianadlr01@gmail.com>
2023-12-18 22:02:38 +0100

    now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
